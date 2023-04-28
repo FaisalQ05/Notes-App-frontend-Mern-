71ebed555d3d59371ef05657ab7a07eae298352e
faisalqureshi05 <faisal199605@gmail.com>
2023-04-28 20:33:40 +0500

    Developement setup Links

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://determined-frog-capris.cyclic.app',
+    // baseUrl: 'https://determined-frog-capris.cyclic.app',
+    baseUrl: 'http://localhost:3500',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
